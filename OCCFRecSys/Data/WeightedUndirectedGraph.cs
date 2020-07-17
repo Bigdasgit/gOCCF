0ac2078215902f3aa42f9d2992885f3f4d97d11f
yeonchang <lyc0324@gmail.com>
2020-07-17 19:33:33 +0900

    init

---
+++ OCCFRecSys/Data/WeightedUndirectedGraph.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCCFRecSys
+{
+    public class WeightedUndirectedGraph
+    {
+        public Dictionary<int, HashSet<Weight>> graph;
+        public HashSet<int> fromids;                        
+        public int Size;
+        public int Edges;
+        public int user_size;
+        private double MinWeight = double.MaxValue;
+        private double MaxWeight = double.MinValue;
+
+        /// <param name="k"># of nearest neighbors</param>
+        /// <param name="e">threshold of weight</param>
+        public WeightedUndirectedGraph(List<WeightedLink> Links, double e)
+        {
+            int size = -1, edges = 0;
+            user_size = -1;
+
+            graph = new Dictionary<int, HashSet<Weight>>();
+            fromids = new HashSet<int>();
+
+            // build graph(adjacency list) from links
+            foreach (WeightedLink i in Links)
+            {
+                // if the row not exists
+                if (!fromids.Contains(i.x))
+                {
+                    fromids.Add(i.x);
+                    graph.Add(i.x, new HashSet<Weight>());
+                }
+                if (!fromids.Contains(i.y))
+                {
+                    fromids.Add(i.y);
+                    graph.Add(i.y, new HashSet<Weight>());
+                }
+
+                // find size(last node id) of graph
+                if (size < i.x)
+                    size = i.x;
+                if (size < i.y)
+                    size = i.y;
+
+                if (user_size < i.x)
+                    user_size = i.x;
+
+                graph[i.x].Add(new Weight(i.y, i.w));
+                graph[i.y].Add(new Weight(i.x, i.w));
+                if (i.w > MaxWeight)
+                    MaxWeight = i.w;
+                if (i.w < MinWeight)
+                    MinWeight = i.w;
+                edges++;
+            }
+            this.Edges = edges;
+            this.Size = size + 1;
+            this.user_size += 1;
+        }
+
+        public WeightedUndirectedGraph(List<WeightedLink> Links)
+        {
+            int size = -1, edges = 0;
+            user_size = -1;
+
+            graph = new Dictionary<int, HashSet<Weight>>();
+            fromids = new HashSet<int>();
+
+            // build graph(adjacency list) from links
+            foreach (WeightedLink i in Links)
+            {
+                // if the row not exists
+                if (!fromids.Contains(i.x))
+                {
+                    fromids.Add(i.x);
+                    graph.Add(i.x, new HashSet<Weight>());
+                }
+                if (!fromids.Contains(i.y))
+                {
+                    fromids.Add(i.y);
+                    graph.Add(i.y, new HashSet<Weight>());
+                }
+
+                // find size(last node id) of graph
+                if (size < i.x)
+                    size = i.x;
+                if (size < i.y)
+                    size = i.y;
+
+                if (user_size < i.x)
+                    user_size = i.x;
+
+                graph[i.x].Add(new Weight(i.y, i.w));
+                graph[i.y].Add(new Weight(i.x, i.w));
+                if (i.w > MaxWeight)
+                    MaxWeight = i.w;
+                if (i.w < MinWeight)
+                    MinWeight = i.w;
+                edges++;
+            }
+            this.Edges = edges;
+            this.Size = size + 1;
+            this.user_size += 1;
+        }
+
+        public HashSet<Weight> GetOutlinks(int i)
+        {
+            if (fromids.Contains(i))
+                return graph[i];
+            else
+                return null;
+        }
+    }
+
+    public struct WeightedLink
+    {
+        public WeightedLink(int a, int b, double c)
+        {
+            x = a;
+            y = b;
+            w = c;
+        }
+
+        public int x;
+        public int y;
+        public double w;
+
+        public static int CompareWeightedLink(WeightedLink a, WeightedLink b)
+        {
+            int result = a.w.CompareTo(b.w);
+
+            if (result == 0)
+                return a.x.CompareTo(b.x);
+            else
+                return result;
+        }
+        public static int CompareWeightedLinkDesc(WeightedLink a, WeightedLink b)
+        {
+            int result = b.w.CompareTo(a.w);
+
+            if (result == 0)
+                return a.x.CompareTo(b.x);
+            else
+                return result;
+        }
+    }
+
+    public struct Weight
+    {
+        public Weight(int ID, double Weight)
+        {
+            id = ID;
+            w = Weight;
+        }
+
+        public int id;
+        public double w;
+
+        public static int CompareWeight(Weight a, Weight b)
+        {
+            int result = a.w.CompareTo(b.w);
+
+            if (result == 0)
+                return a.id.CompareTo(b.id);
+            else
+                return result;
+        }
+        public static int CompareWeightDesc(Weight a, Weight b)
+        {
+            int result = b.w.CompareTo(a.w);
+
+            if (result == 0)
+                return a.id.CompareTo(b.id);
+            else
+                return result;
+        }
+        public static int CompareID(Weight a, Weight b)
+        {
+            return a.id.CompareTo(b.id);
+        }
+    }
+}
---
+++ OCCFRecSys/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace OCCFRecSys
+{
+    class Program
+    {
+        public static string file_path;
+        public static StreamReader sr;
+        public static StreamWriter sw;
+        public static HashSet<int> tophead_items;
+        public static int num_user;
+        //public static double[] betas = { 0.9 };
+        //public static double[] gammas = { 0.9 };
+        //public static double beta = 0d;
+        //public static double gamma = 0d;
+
+        static void Main(string[] args)
+        {           
+            string[] candidate_items = { "All_items" }; //  "All_items", "Longtail_items"
+            string[] algorithms = { "SignedRWR" }; //"RWR", "SeparateRWR", "SignedRWR", "SeparateBP", "SignedBP"
+
+            //file_path = "D:\\Downloads\\DataFile\\gOCCF\\Movielens\\";
+            //num_user = 943;
+            //foreach (string algo in algorithms)
+            //    foreach (string candidate_item in candidate_items)
+            //        Experiments(algo, candidate_item);
+
+            file_path = "D:\\Downloads\\DataFile\\gOCCF\\Watcha\\";
+            num_user = 1391;
+            foreach (string algo in algorithms)
+                foreach (string candidate_item in candidate_items)
+                    Experiments(algo, candidate_item);
+
+
+            //file_path = "D:\\Downloads\\DataFile\\Ciao\\";
+            //num_user = 996;
+            //foreach (string algo in algorithms)
+            //    foreach (string candidate_item in candidate_items)
+            //        Experiments(algo, candidate_item);
+
+            //file_path = "D:\\Downloads\\DataFile\\CiteULike\\";
+            //num_user = 5551;
+            //foreach (string algo in algorithms)
+            //    foreach (string candidate_item in candidate_items)
+            //        Experiments(algo, candidate_item);
+
+
+            //foreach (double beta in betas)
+            //    foreach (double gamma in gammas)
+            //        foreach (string algo in algorithms)
+            //            foreach (string candidate_item in candidate_items)
+            //            {
+            //                Program.beta = beta;
+            //                Program.gamma = gamma;
+            //                Experiments(algo, candidate_item);
+            //            }
+        }
+
+        public static void Experiments(string algo, string candidate_item)
+        {         
+            int num_recommend = 50;
+            for (int fold = 1; fold <= 1; fold++)
+            {
+                if (candidate_item == "Longtail_items")
+                {
+                    tophead_items = new HashSet<int>();
+                    sr = new StreamReader(file_path + "raw\\longtail\\u" + fold + "_longtail_items.txt");
+                    while (!sr.EndOfStream)
+                    {
+                        int item_id = int.Parse(sr.ReadLine().ToString()) + num_user;
+                        tophead_items.Add(item_id);
+                    }
+                    sr.Close();
+                }
+
+                StreamReader training = new StreamReader(file_path + "unint\\basic\\u" + fold + "\\u" + fold + "_balance.base");
+                StreamReader test = new StreamReader(file_path + "raw\\basic\\u" + fold + "\\u" + fold + ".test");
+                StreamWriter result = new StreamWriter(file_path + "results\\" + algo + "\\unint\\origin\\u" + fold + "_" + algo + "_rankresult_balance(" + candidate_item + ").results");
+                StreamWriter time = new StreamWriter(file_path + "results\\" + algo + "\\unint\\origin\\u" + fold + "_" + algo + "_rankresult_balance(" + candidate_item + ").time");
+
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                if (algo == "SeparateRWR")
+                {
+                    var recsys = new SeparateRWR_Recommender(candidate_item, num_recommend, fold, training, test);
+                    recsys.Recommend();
+                    recsys.PrintResults(result);
+                }
+                else if (algo == "SignedRWR")
+                {
+                    double beta = 0.9d;
+                    double gamma = 0.9d;                   
+                    var recsys = new SignedRWR_Recommender(candidate_item, num_recommend, fold, training, test, beta, gamma);
+                    recsys.Recommend();
+                    recsys.PrintResults(result);
+                }
+                else if (algo == "SeparateBP")
+                {
+                    int num_iter = 5;
+                    double propagation_alpha = 0.0001d;
+                    var recsys = new SeparateBP_Recommender(candidate_item, num_iter, num_recommend, propagation_alpha, fold, training, test);
+                    recsys.Recommend();
+                    recsys.PrintResults(result);
+                }
+                else if (algo == "SignedBP")
+                {
+                    int num_iter = 5;
+                    double propagation_alpha = 0.0001d;
+                    var recsys = new SignedBP_Recommender(candidate_item, num_iter, num_recommend, propagation_alpha, fold, training, test);
+                    recsys.Recommend();
+                    recsys.PrintResults(result);
+                }
+                else if (algo == "RWR")
+                {
+                    var recsys = new RWR_Recommender(candidate_item, num_recommend, fold, training, test);
+                    recsys.Recommend();
+                    recsys.PrintResults(result);
+                }
+                sw.Stop();
+                time.WriteLine(algo + "\t" + sw.ElapsedMilliseconds.ToString() + "ms");
+                Console.WriteLine(algo + "\t" + sw.ElapsedMilliseconds.ToString() + "ms");
+
+                time.Close();
+                result.Close();
+            }
+        }        
+    }
+
+    public class Pairs
+    {
+        public Pairs(int user_id, int item_id,double score)
+        {
+            this.user_id = user_id;
+            this.item_id = item_id;
+            this.score = score;
+        }
+
+        public int user_id;
+        public int item_id;
+        public double score;
+
+        public static int CompareScore(Pairs a, Pairs b)
+        {
+            int result = a.score.CompareTo(b.score);
+
+            return result;
+        }
+    }
+
+
+
+}
